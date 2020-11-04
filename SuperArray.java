d8b25f59a6d02a258cdad3f8c6cc7de413227936
Kirsten Szeto <kszeto20@stuy.edu>
2020-11-04 00:28:08 -0500

    Added get(), set(), resize(), modified add(). Everything works.

--- SuperArray.java
+++ SuperArray.java
@@ -2,19 +2,28 @@ public class SuperArray {
 
   private String[] data;
   private int size; // current size
+  private int capacity;
 
   public SuperArray() {
     data = new String[10];
     size = 0;
+    capacity = 10;
   }
 
   public int size() {
     return size;
   }
 
+  //public int cap() {
+    //return capacity;
+  //}
+
   public boolean add(String element) {
-    if (size >= 10) {
-      return false;
+    if (size >= capacity) {
+      resize();
+      data[size] = element;
+      size++;
+      return true;
     }
     else {
       data[size] = element;
@@ -23,4 +32,22 @@ public class SuperArray {
     }
   }
 
+  public String get(int index) {
+    return data[index];
+  }
+
+  public String set(int index, String element) {
+    String orig = data[index];
+    data[index] = element;
+    return orig;
+  }
+
+  private void resize() {
+    String[] bigData = new String[size + 10];
+    for (int i = 0; i < size; i++) {
+      bigData[i] = data[i];
+    }
+    data = bigData;
+    capacity += 10;
+  }
 }
