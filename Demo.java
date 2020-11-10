f78c2498676adb4f292d885bb4656b573a99e555
Kirsten Szeto <kszeto20@stuy.edu>
2020-11-10 00:02:53 -0500

    removeDuplicates is good now ! Also I finished adding findOverlap

--- Demo.java
+++ Demo.java
@@ -1,28 +1,45 @@
 public class Demo {
 
   public static void removeDuplicates (SuperArray s) {
-
     for (int i = 0; i < s.size(); i++) {
       String comp = s.get(i);
-      for (int j = i + 1; j < s.size(); j++) {
-        if (s.get(j).equals(comp)) {
+      for (int j = s.size() - 1; j > i; j--) {
+        if (comp.equals(s.get(j))) {
           s.remove(j);
         }
       }
     }
   }
+
+  public static SuperArray findOverlap(SuperArray a, SuperArray b) {
+    removeDuplicates(a);
+    removeDuplicates(b);
+    SuperArray cross = new SuperArray();
+    for (int i = 0; i < a.size(); i++) {
+      String str = a.get(i);
+      for (int j = 0; j < b.size(); j++) {
+        if (b.get(j).equals(str)) {
+          cross.add(b.get(j));
+        }
+      }
+    }
+    return cross;
+  }
+  
+
   public static void main(String[]args){
     SuperArray words = new SuperArray();
-    //grouped to save vertical space
+
     words.add("kani");   words.add("uni");
     words.add("ebi");    words.add("una");
     words.add("una");    words.add("ebi");
     words.add("kani");   words.add("una");
     words.add("una");    words.add("ebi");
-    words.add("toro");
+    words.add("toro");   words.add("una");
 
     System.out.println(words);
     removeDuplicates(words);
     System.out.println(words);
+
   }
 }
